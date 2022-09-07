c9027b682f3fce457c496d6be323528c3726d67d
daniele.aloisio <danielealoisio@hotmail.it>
2022-09-07 15:20:13 +0200

    log

---
+++ CommonCloud.Log/Dto/ErrDto.cs
@@ -0,0 +1,17 @@
+﻿namespace CommonCloud.API.Dto
+{
+    public class ErrDto
+    {
+        public ErrDto(string message, int errcode)
+        {
+            this.message = message;
+            this.errcode = errcode;
+            this.data = DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+        public string message { get; set; }
+        public int errcode { get; set; }
+        public string data { get; set; }
+
+    }
+}
---
+++ CommonCloud.Log/Dto/InfoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CommonCloud.API.Dto
+{
+    public class InfoDto
+    {
+        public string message { get; set; }
+
+        public InfoMsg(String Message)
+        {
+            this.message = Message;
+        }
+    }
+}
