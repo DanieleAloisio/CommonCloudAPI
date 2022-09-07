ed2255efe7263a81e794974120e3770b6c733a71
daniele.aloisio <danielealoisio@hotmail.it>
2022-09-07 17:47:33 +0200

    middleware

---
+++ CommonCloud.Log/LoggingMiddleware.cs
@@ -0,0 +1,62 @@
+﻿using CommonCloud.Repository.Models;
+using Microsoft.AspNetCore.Http;
+using System.Text;
+
+namespace CommonCloud.Log
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            //First, get the incoming request
+            var request = await FormatRequest(context.Request);
+            var response = await FormatResponse(context.Response);
+
+            LogModel logModel = new LogModel();
+            logModel.Request = request;
+            logModel.Response = response;
+        }
+
+        private async Task<string> FormatRequest(HttpRequest request)
+        {
+            var body = request.Body;
+
+            //We now need to read the request stream.  First, we create a new byte[] with the same length as the request stream...
+            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
+
+            //...Then we copy the entire request stream into the new buffer.
+            await request.Body.ReadAsync(buffer, 0, buffer.Length);
+
+            //We convert the byte[] into a string using UTF8 encoding...
+            var bodyAsText = Encoding.UTF8.GetString(buffer);
+
+            //..and finally, assign the read body back to the request body, which is allowed because of EnableRewind()
+            request.Body = body;
+
+            return $"{request.Scheme} {request.Host}{request.Path} {request.QueryString} {bodyAsText}";
+        }
+
+        private async Task<string> FormatResponse(HttpResponse response)
+        {
+            //We need to read the response stream from the beginning...
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            //...and copy it into a string
+            string text = await new StreamReader(response.Body).ReadToEndAsync();
+
+            //We need to reset the reader for the response so that the client can read it.
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            //Return the string for the response, including the status code (e.g. 200, 404, 401, etc.)
+            return $"{response.StatusCode}: {text}";
+        }
+    }
+}
+
